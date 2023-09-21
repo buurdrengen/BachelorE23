e4df83bff8dc51515ba093ab3a812818283f753f
karenwjensen <113354172+karenwjensen@users.noreply.github.com>
2023-09-21 10:17:14 +0200

    Update karen tester modeller.jl

--- karen tester modeller.jl
+++ karen tester modeller.jl
@@ -7,13 +7,15 @@ m = Model(HiGHS.Optimizer);
 
 @variable(m, X[1:2] >= 0) 
 
-@objective(m, Max, 12*X[1] + 30*X[2]) 
+@objective(m, Max, 2.5*X[1] + 4*X[2]) 
 
-@constraint(m, X[1] + 2*X[2] <= 230)
+@constraint(m, 5*X[1] + 10*X[2] <= 230)
 @constraint(m, 3*X[1] + 5*X[2] <= 230) 
-@constraint(m, 7*X[1] + 17*X[2] <= 230)
-@constraint(m, 7*X[1] + 14*X[2] <= 230)
-@constraint(m, 3*X[1] + 6*X[2] <= 230)
+@constraint(m, 3*X[1] + 3*X[2] <= 230)
+@constraint(m, X[1] + 2*X[2] <= 230)
+@constraint(m, 10*X[1] + 10*X[2] <= 230)
+@constraint(m, X[1]<= 30)
+@constraint(m, X[2]<= 40)
 
 println(m)
 optimize!(m)
