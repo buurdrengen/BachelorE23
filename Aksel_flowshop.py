bf1794bc3134f8aca88f187bf3febb31e3819bcf
buurdrengen <s203947@student.dtu.dk>
2023-09-24 10:21:12 +0200

    flowshp

--- Aksel_flowshop.py
+++ Aksel_flowshop.py
@@ -14,9 +14,22 @@ print(total_jobs)
 total_completion_time = total_jobs.sum(axis=1)
 print(total_completion_time)
 
-best_order = np.array([])
-
 n = total_completion_time.shape[0]
+
+def heuristics_algorithm(total_jobs,total_completion_time):
+    n = total_completion_time.shape[0]
+    i = 3 
+    while i < (n+1): 
+        perms = create_permutations(best_order,total_completion_time[i-1])
+        c_max = np.inf
+        for job in perms: 
+            current_c_max = compute_makespan(job)
+            if current_c_max < c_max:
+                c_max = current_c_max
+                best_order = job 
+        i += 1 
+        print(c_max,best_order)
+    return best_order
 
 
 def create_permutations(list_jobs, job):
@@ -39,7 +52,8 @@ def create_permutations(list_jobs, job):
         return list_permutations
 
 #print(create_permutations(np.array([1,2]),3))
-
-#def compute_makespan()
-
 #print(np.size(create_permutations(total_completion_time,1)))
+
+def compute_makespan(total_jobs, list_jobs)
+    
+
