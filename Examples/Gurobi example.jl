161ccaa7b7f86ef5d60260246f12366159455388
buurdrengen <s203947@student.dtu.dk>
2023-09-28 15:28:25 +0200

    gurobi example

---
+++ Examples/Gurobi example.jl
@@ -0,0 +1,33 @@
+############################
+# For adding Gurobi to julia
+##
+# using Pkg
+# Pkg.add("Gurobi")
+############################
+
+# Problem: 
+# Min x + y 
+# s.t. 
+# 5 <= x + 2y <= 15
+# 6 <= 3x + 2y 
+# 0 <= x <= 4
+# 1 <= y
+# y in Z (y is integer)
+
+using JuMP 
+using Gurobi
+
+m = Model(Gurobi.Optimizer)
+
+@variable(m, 0 <= x <= 4)
+@variable(m, 1 <= y, Int)
+
+@objective(m, Min, (x + y))
+@constraint(m, 5 <= (x + 2*y) <= 15)
+@constraint(m, 6 <= (3*x + 2*y))
+
+optimize!(m)
+
+println("Objective value: ",JuMP.objective_value(m))
+println("x: ",JuMP.value(x))
+println("y: ",JuMP.value(y))
