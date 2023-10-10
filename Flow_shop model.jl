369d719e9cf8729cb25aae17a283ccc15eafbf0a
karenwjensen <113354172+karenwjensen@users.noreply.github.com>
2023-10-10 15:39:27 +0200

    flow_shop_loop

--- Flow_shop model.jl
+++ Flow_shop model.jl
@@ -6,7 +6,7 @@ model = Model(Gurobi.Optimizer);
 
 No_products = [[1,2] [2,3] [3,4] [4,5] [5,6] [6,7] [10,11]]
 #  optimizer(No_products)
-
+# model
 # function optimizer(No_products)
 
 p1 = [5, 3, 3, 1, 10]  
