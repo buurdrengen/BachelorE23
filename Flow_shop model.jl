fcd1a8db2c89de9f5820d98ab3b0e14feec8ae54
buurdrengen <s203947@student.dtu.dk>
2023-10-12 12:42:17 +0200

    soltime

--- Flow_shop model.jl
+++ Flow_shop model.jl
@@ -4,7 +4,7 @@ using Plots
 
 model = Model(Gurobi.Optimizer); 
 
-No_products = [[1,2] [2,3] [3,4] [4,5] [5,6] [6,7] [10,11]]
+No_products = [[1,2] [2,3] [3,4] [4,5] [5,6] [6,7] [7,8]]
 #  optimizer(No_products)
 # model  
 # function optimizer(No_products)
